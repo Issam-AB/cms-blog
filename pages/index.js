09834589c790dfe6c57029f04768275380e42b18
Issam AB <Issam.aboulfadl05@gmail.com>
2021-11-05 22:42:38 +0100

    initaill commit

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">x
+      
+      
+      
+      xsx s</div>
+    </div>
+  );
+}
